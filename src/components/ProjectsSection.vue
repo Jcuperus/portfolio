d59b1f03dcf65b979f170e892951dec0142c39dc
Jaep Cuperus <jaepcuperus@hotmail.com>
2026-02-19 19:51:34 +0100

    Refactored experience data processing into composable

--- src/components/ProjectsSection.vue
+++ src/components/ProjectsSection.vue
@@ -3,31 +3,15 @@ import ExperienceList from "@/components/Experience/ExperienceList.vue";
 
 import ArticleTitle from "@/components/ArticleTitle.vue";
 import FilterBox from "@/components/Experience/FilterBox.vue";
-import {
-  experience_items as all_experience_items,
-  queryExperienceItem,
-} from "@/models/experience_item";
-import { ref } from "vue";
+import { useExperienceItems } from "@/composables/useExperienceItems.ts";
 
-const experience_items = ref(all_experience_items);
-
-function filterProjects(query?: string) {
-  if (!query || query === "") {
-    experience_items.value = all_experience_items;
-  } else {
-    const filtered_items = all_experience_items.filter((exp_item) =>
-      queryExperienceItem(exp_item, query),
-    );
-
-    experience_items.value = filtered_items;
-  }
-}
+const { experience_items, filter } = useExperienceItems();
 </script>
 
 <template>
   <section>
     <ArticleTitle title="projecten" id="projects" />
-    <FilterBox @change="filterProjects" />
+    <FilterBox @change="filter" />
     <ExperienceList :experience_items="experience_items" />
   </section>
 </template>
